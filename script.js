0b6c59992f3c7d9969d892655e27f6af73180327
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-06-27 18:26:19 -0400

    intergrating openai API

---
+++ script.js
@@ -0,0 +1,8 @@
+import { config } from "dotenv"
+config()
+
+import { Configuration, OpenAIApi } from "openai"
+
+const openai =new OpenAIApi(new Configuration({
+    apiKey: process.env.API_KEY
+}))
