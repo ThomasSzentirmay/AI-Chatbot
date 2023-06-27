50504b5f484be9d564d6563d6ee2c783c3dd87d7
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-06-27 18:38:18 -0400

    finished node AI chat model

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ import readline from "readline"
 
 const openai =new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY
-}))
+}));
 
 const userInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
 userInterface.prompt()
+userInterface.on("line", async input => {
+   const res = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: input}]
+    });
 
-openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello ChatGPT"}]
-}).then(res => {
     console.log(res.data.choices[0].message.content);
-})
+    userInterface.prompt();
+});
