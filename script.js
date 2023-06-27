bf5b1799602af012c700132616207c1d203e0157
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-06-27 18:32:39 -0400

    added user prompt to ask AI questions

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ import { config } from "dotenv"
 config()
 
 import { Configuration, OpenAIApi } from "openai"
+import readline from "readline"
 
 const openai =new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY
 }))
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+userInterface.prompt()
+
+openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello ChatGPT"}]
+}).then(res => {
+    console.log(res.data.choices[0].message.content);
+})
